57e92dd74179dfac454f46a54130adcc7a8ba710
Reyonaes <140374133+Reyonaes@users.noreply.github.com>
2023-12-06 19:21:10 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import cv2
+from tensorflow import keras
+uploaded_img = st.file_uploader("Upload an image to detect emotion", type= ['png', 'jpg', 'jpeg'])
+model=keras.models.load_model("Emotion_Model.h5")
+emotions=["angry","disgust","fear","happy","neutral","sad","surprise"]
+if uploaded_img is not None:
+    with open("img.jpg",'wb') as f:
+        f.write(uploaded_img.read())
+img=cv2.imread("img.jpg",0)
+img=np.array(img)
+img=img.reshape(img.shape[0],48,48,1)
+ind=model.predict(img)
+st.write("The emotion displayed is ",emotions[ind])
