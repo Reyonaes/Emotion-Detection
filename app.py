d10595ce219501c91af671cc3c612eceddc10e8a
Reyonaes <140374133+Reyonaes@users.noreply.github.com>
2023-12-07 02:47:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import numpy as np
 uploaded_img = st.file_uploader("Upload an image to detect emotion", type= ['png', 'jpg', 'jpeg'])
 model=keras.models.load_model("Emotion_Model2.h5")
 emotions=["Angry","Disgust","Fear","Happy","Neutral","Sad","Surprise"]
-if uploaded_img is not None:
-    with open("img.jpg",'wb') as f:
-        f.write(uploaded_img.read())
-img=cv2.imread("img.jpg",0)
+#if uploaded_img is not None:
+    #with open("img.jpg",'wb') as f:
+        #f.write(uploaded_img.read())
+img=cv2.imread(uploaded_img,0)
 img=np.array(img)
 img=img.reshape(img.shape[0],48,48,1)
 ind=model.predict(img)
